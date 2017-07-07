46d2a203eebe4303cf3eef8d373d0f9f1c7c6434
Holymir <vtsochev@abv.bg>
2017-07-07 15:18:59 +0300

    Add files via upload

---
+++ 04. Triple Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Triple_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int lenght = nums.Length;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = i + 1; j < lenght; j++)
+                {
+                    for (int k = 0; k < lenght; k++)
+                    {
+                        int result = nums[k];
+                        if (nums[i] + nums[j] == result)
+                        {
+                            Console.WriteLine($"{nums[i]} + {nums[j]} == {result}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
